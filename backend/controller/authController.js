ef122ad5b8a5d8bb0801872afb199d2b512b9702
avdyldemukaj <lumi.demukaj2@gmail.com>
2024-03-18 23:57:19 +0100

    "validating signup"

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -2,6 +2,13 @@ const { User } = require("../models");
 const signup = async (req, res, next) => {
   try {
     const { name, email, password, role } = req.body;
+
+    const isEmailExist = await User.findOne({ email });
+    if (isEmailExist) {
+      res.code = 400;
+      throw new Error("Email alredy existe!");
+    }
+
     const newUser = new User({ name, email, password, role });
     await newUser.save();
     res.status(201).json({
