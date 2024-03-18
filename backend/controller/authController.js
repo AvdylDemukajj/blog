e960581a628ae917c5bedf8415defa972bf4e901
avdyldemukaj <lumi.demukaj2@gmail.com>
2024-03-18 23:24:27 +0100

    "Signup validation"

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -2,6 +2,22 @@ const { User } = require("../models");
 const signup = async (req, res, next) => {
   try {
     const { name, email, password, role } = req.body;
+
+    if (!name) {
+      res.code = 400;
+      throw new Error("Name is required");
+    }
+    if (!email) {
+      res.code = 400;
+      throw new Error("Email is required");
+    }
+    if (!password) {
+      res.code = 400;
+      throw new Error("Password is required");
+    }
+    if (password.length < 6) {
+      throw new Error("Password should be 6 characters long");
+    }
     const newUser = new User({ name, email, password, role });
     await newUser.save();
     res.status(201).json({
